e7732638af905b1654f39c6f38a23039ea7ce9ff
athirashaji29 <118505212+athirashaji29@users.noreply.github.com>
2023-02-16 06:16:30 +0000

    lab3 updates

---
+++ lab3/lab3sol.java
@@ -0,0 +1,86 @@
+import java.util.*;
+class lab3sol{
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        // collecting  integer array from the user 
+        System.out.println("enter the size of array");
+        int size = scan.nextInt();
+        int[] array=new int[size];
+        System.out.println("enter the values for the array");
+        for(int i = 0 ; i < size ; i++){
+            array[i]=scan.nextInt();
+        }
+        // display menu of item and choosing
+        System.out.println("1. Mean: ");
+        System.out.println("2.Median: " ) ;  
+        System.out.println("3.Mode: ") ;
+        System.out.println("select any of the option: ") ;
+        int option = scan.nextInt();
+        switch(option){
+            case 1: float mean= fnmean(array);
+            System.out.println(" mean is: " + mean);
+            break;
+            case 2: float median = fnmedian(array);
+            System.out.println("median is : " + median);
+            break;
+            case 3: int mode =fnmode(array);
+            System.out.println("mode is" + mode);
+            break;
+            default:
+            System.out.println(" invalid option");
+        }
+        scan.close();
+
+
+    }
+    //first function for mean
+    static float fnmean(int[]array){
+        int sum=0;
+        for(int i=0 ; i < array.length ; i++){
+            sum +=array[i];
+            }
+            return (float) sum/array.length;
+    }
+    //second function for median
+    static float fnmedian(int[]array){
+        Arrays.sort(array);
+        if(array.length%2==0){
+            int midvalue1= array.length/2 - 1; 
+            int midvalue2=array.length/2;
+            return( float) (array[midvalue1]+array[midvalue2])/2;
+        }else{
+            int midvalue=array.length/2;
+            return(float)(array[midvalue]);
+        }
+
+    }
+    public static int fnmode(int[] array) {
+    // Sort the array in ascending order
+    Arrays.sort(array);
+
+    int mode = array[0];
+    int count = 1;
+    int maxCount = 1;
+
+    // Iterate over the sorted array, comparing adjacent elements
+    for (int i = 1; i < array.length; i++) {
+        if (array[i] == array[i-1]) {
+            // If the current element is the same as the previous element, increment count
+            count++;
+        } else {
+            // If the current element is different from the previous element, reset count to 1
+            count = 1;
+        }
+
+        // If the current count is greater than the maximum count, update the mode
+        if (count > maxCount) {
+            maxCount = count;
+            mode = array[i];
+        }
+    }
+
+    return mode;
+    }
+}
+   
+    
