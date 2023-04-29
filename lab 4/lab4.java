b5d035b576f7102dab287351d8475f6b1676b84d
ATHIRA SHAJI <118505212+athirashaji29@users.noreply.github.com>
2023-04-29 12:11:41 +0530

    Update lab4.java

--- lab 4/lab4.java
+++ lab 4/lab4.java
@@ -1,100 +1,108 @@
+// Write a JAVA Menu driven program that does the following:
+// You can ONLY have the below variables as global variables
+// Account Number
+// Account Holder Name
+// Account Balance
+// You MUST have the below as functions
+// To initialize the customer
+// To deposit money
+// To withdraw money
+// To print the transactions
+// To print account summary
+// Your menu will have the following operations once the customer is created
+// To deposit money
+// To withdraw money
+// To print the transactions
+// To print account summary
+// You need to draw a flowchart and structure your results and program description, and other details in README.md.
 import java.util.*;
-public class lab4{
-    static int accnt no;
-    static String accnt name;
-    static int accnt balance;
-    static scanner scan= new scanner(System.in);
-    static ArrayList<String> transactions = new ArrayList<>();
-    public static void initializeCustomer(int accntNo, String accntHolderName, double accmntBalance) {
-        accountNumber = accntNo;
-        accountHolderName = accntHolderName;
-        accountBalance = accntBalance;
+ public class la4 {
+
+    private static int accountNumber ;
+    private  static String accountHolderName;
+    private static float accountBalance;
+    private static String transaction;
+
+    public void initialize (int accNum,String acholname,float accoBal){
+        accountNumber = accNum;
+        accountHolderName= acholname;
+        accountBalance=accoBal;
+        transaction = "";
+
     }
+    public static void deposit(float money){
+        accountBalance += money;
+        transaction +="deposited : "+ money+ "\n";
 
 
+
+    }
     
-    // function to deposite money into account
-    public void deposit( float amount){
-        accntbalance += amount;
-        transaction.add("updated balance is" + accntbalance);
-    }
+    public static void withdraw (float money){
+        if ( accountBalance<money){
+            System.out.println("not balance");
 
-    // function to withdraw money frpm account
-    public void withdraw(float amount){
-        if(accntbalance< amount){
-            System.out.println(" insufficient balance: ")
         }
         else{
-            accntbalance -=amount;
-            transaction.add("updated balance is "+ accntbalance);
+            accountBalance -= money;
+            transaction += "withdraw : " + money + "\n";
 
         }
     }
-    // function to print the transaction 
-    public void transaction (){
+    public static void transactions (){
+        System.out.println("transactions: "+transaction+"\n") ;
+    }
+    public static void summary (String acholname,int accNum){
+        System.out.println("account holder name "+ acholname+"\n");
+        System.out.println("account no"+accNum+ "\n");
+        System.out.println("transactions" + transaction+ "\n");
+    }
+    public static void main(String [] args){
+        int choice;
+        Scanner sc = new Scanner(System.in);
+         
+        System.out.println("enter account holder name");
+        String acholname = sc.nextLine();
+        System.out.println("enter account no ");
+        int accNum = sc.nextInt();
+        // System.out.println("account balance ");
+        float accoBal = 5000;
+        float money;
+        do{
        
-        for (String transaction : transactions) {
-            System.out.println(transaction);
-        }
-    }
+        System.out.println("enter your choice \n1.deposite money \n2.withdrawmoney \n3.to print transaction \n4.to print account summary");
+        choice = sc.nextInt();
+        switch(choice){
+            case 1: 
+            System.out.print("enter the money you wish to deposite : ");
+            money = sc.nextInt();
+            deposit(money);
+            System.out.println("deposited ");
+            break;
 
-    // function to print the account summary
-    public void accntsummary(){
-        System.out.println("Account Number" + accountNumber );
-        System.out.println("Account Holder Name " + accountHolderName);
-        System.out.println("ACcount balance " + accntbalance);
-    }
-    
-    public static void main (static[] args){
-    System.scan=new scanner(System.in);
-    // Initialize the customer
-        initializeCustomer(6466, "Athira Shaji", 2589);
 
-        // Menu-driven program
-        boolean quit = false;
-    while(true){
-        System.out.println(1.To deposit);
-        System.out.println(2.To withdraw);
-        System.out.println(3.To print the account transaction);
-        System.out.println(4.To print Account summary);
-        System.out.println(5.Exit);
-        System.out.print("Enter your choice: ");
-        int(option)= scanner.nextInt();
-        switch(option){
-            case 1:
-                System.out.println(" enter the amount you wish to deposite:")
-                float depositAmount = scanner.nextfloat();
-                deposit(depositAmount);
-                break;
-            
             case 2:
-                System.out.print("Enter withdrawal amount: ");
-                float withdrawalAmount = scanner.nextfloat();
-                withdraw(withdrawalAmount);
-                break;
-            case 3:
-                printTransactions();
-                break;
-            case 4:
-                printAccountSummary();
-                break;
-             case 5:
-                    quit = true;
-                    break;
-                default:
-                    System.out.println("Invalid choice.");
+            System.out.println("enter the ammount for withdraw");
+            money = sc.nextInt();
+            withdraw(money);
+            System.out.println("withdrawed");
+            break;
 
-            
+            case 3:
+            transactions ();
+            break;
+            case 4:
+            summary(acholname,accNum);
+            break;
+
         }
 
+
+        }while(choice !=0);
+        sc.close();
+
+
+
     }
-     scanner.close();
-
-
-    }
-
-
-
-
-
-}
+   
+ }
