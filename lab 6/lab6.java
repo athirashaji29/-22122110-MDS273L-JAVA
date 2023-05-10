d84587f7c78b0fdf6f95e669d43f58782f0e3c7e
ATHIRA SHAJI <118505212+athirashaji29@users.noreply.github.com>
2023-05-10 12:09:35 +0000

    lab6 updates

--- lab 6/lab6.java
+++ lab 6/lab6.java
@@ -43,6 +43,114 @@ public class lab6 {
         } 
         return counts;
     }
+    public static String[] getTopWords(String temp, int n) {
+        String[] words = temp.split("[\\W]+");
+        String[] topWords = new String[n];
+        int[] counts = new int[words.length];
+        for (int i = 0; i < words.length; i++) {
+            counts[i] = 1;
+            for (int j = i + 1; j < words.length; j++) {
+if (words[i].equalsIgnoreCase(words[j])) {
+counts[i]++;
+}
+}
+}
+for (int i = 0; i < n; i++) {
+int maxIndex = getMaxIndex(counts);
+topWords[i] = words[maxIndex];
+counts[maxIndex] = -1;
+}
+return topWords;
+}
+public static String[] getLeastWords(String temp, int n) {
+    String[] words = temp.split("[\\W]+");
+    String[] leastWords = new String[n];
+    int[] counts = new int[words.length];
+    for (int i = 0; i < words.length; i++) {
+        counts[i] = 1;
+        for (int j = i + 1; j < words.length; j++) {
+            if (words[i].equalsIgnoreCase(words[j])) {
+                counts[i]++;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        int minIndex = getMinIndex(counts);
+        leastWords[i] = words[minIndex];
+        counts[minIndex] = Integer.MAX_VALUE;
+    }
+    return leastWords;
+}
+
+public static char[] getTopChars(String temp, int n) {
+    char[] chars = temp.replaceAll("\\s+", "").toCharArray();
+    char[] topChars = new char[n];
+    int[] counts = new int[chars.length];
+    for (int i = 0; i < chars.length; i++) {
+        counts[i] = 1;
+        for (int j = i + 1; j < chars.length; j++) {
+            if (chars[i] == chars[j]) {
+                counts[i]++;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        int maxIndex = getMaxIndex(counts);
+        topChars[i] = chars[maxIndex];
+        counts[maxIndex] = -1;
+    }
+    return topChars;
+}
+
+public static char[] getLeastChars(String temp, int n) {
+    char[] chars = temp.replaceAll("\\s+", "").toCharArray();
+    char[] leastChars = new char[n];
+    int[] counts = new int[chars.length];
+    for (int i = 0; i < chars.length; i++) {
+        counts[i] = 1;
+        for (int j = i + 1; j < chars.length; j++) {
+            if (chars[i] == chars[j]) {
+                counts[i]++;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        int minIndex = getMinIndex(counts);
+        leastChars[i] = chars[minIndex];
+        counts[minIndex] = Integer.MAX_VALUE;
+    }
+    return leastChars;
+}
+
+public static int countChar(String temp, char ch) {
+    int count = 0;
+    for (int i = 0; i < temp.length(); i++) {
+        if (temp.charAt(i) == ch) {
+            count++;
+        }
+    }
+    return count;
+}
+
+public static int getMaxIndex(int[] array) {
+    int maxIndex = 0;
+    for (int i = 1; i < array.length; i++) {
+        if (array[i] > array[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
+}
+
+public static int getMinIndex(int[] array) {
+    int minIndex = 0;
+    for (int i = 1; i < array.length; i++) {
+        if(array[i] < array[minIndex]) {
+minIndex = i;
+}
+}
+return minIndex;
+}
     public static void main(String[] args) {
         String temp="";
          
@@ -59,5 +167,28 @@ public class lab6 {
         }
         Vowelcount(temp);
         digitscount(temp);
+           String[] topWords = getTopWords(temp, 5);
+        String[] leastWords = getLeastWords(temp, 5);
+        char[] topChars = getTopChars(temp, 5);
+        char[] leastChars = getLeastChars(temp, 5);
+        System.out.println("Top 5 repeated words and their count:");
+        for (String word : topWords) {
+            System.out.println(word);
+        }
+
+        System.out.println("Least 5 repeated words and their count:");
+        for (String word : leastWords) {
+            System.out.println(word);
+        }
+
+        System.out.println("Top 5 repeated characters and their count:");
+        for (char ch : topChars) {
+            System.out.println(ch + ": " + countChar(temp, ch));
+        }
+
+        System.out.println("Least 5 repeated characters and their count:");
+        for (char ch : leastChars) {
+            System.out.println(ch + ": " + countChar(temp, ch));
+        }
     }
 }
